4551893d31347e366f946ff89ff1efe00ce1af25
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2023-12-02 00:53:10 +0800

    feat: Added some utils to help me input output

---
+++ pkg/utils/io.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ImportAsArray(path string) (input []string, err error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return []string{}, err
+	}
+
+	input = strings.Split(string(data), "\n")
+	return
+}
+
+func ImportAsString(path string) (input string, err error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	input = string(data)
+	return
+}
+
+func Output(data []byte, path string) {
+	var err error
+	_, err = os.Stat(path)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			// Some unknown error
+			fmt.Printf("Failed to stat file: %s", err.Error())
+			os.Exit(1)
+		}
+	}
+
+	f, err2 := os.Create(path)
+	err = err2
+	if err != nil {
+		// Some error during creating
+		fmt.Printf("Failed to create file: %s", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		fmt.Printf("Failed to write file: %s", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Written data to: %s\n", path)
+	fmt.Printf("Output is %s\n", string(data))
+	os.Exit(0)
+}
